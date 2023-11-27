c02ee1c528c2dd3762ebf57584f307da03ceb349
Conrad Shock <shockconrad@gmail.com>
2023-11-27 15:18:53 -0800

    json

---
+++ learning_js/scripts/json-usage.js
@@ -0,0 +1,14 @@
+async function populate() {
+  const requestURL = "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const superHeroesJson = await response.json();
+  console.log(superHeroesJson);
+  // or const superHeroes = JSON.parse(await response.text());
+
+  let myObj = { name: "Chris", age: 38 };
+  console.log(JSON.stringify(myObj));
+}
+
+populate();
