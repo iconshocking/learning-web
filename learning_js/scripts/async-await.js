b3445edf5cfbafcbd270863478e7c50e978e76de
Conrad Shock <shockconrad@gmail.com>
2023-11-30 13:16:03 -0800

    async and promise learnings

---
+++ learning_js/scripts/async-await.js
@@ -0,0 +1,148 @@
+// async functions always returns a promise, but they will differ slightly depending on what is returned:
+// 1. returns resolved promise with value 1
+async function foo1() {
+  return 1;
+}
+console.log(foo1());
+// 2. returns pending promise which will resolve to value 1
+async function foo2() {
+  return Promise.resolve(1);
+}
+console.log(foo2());
+
+// Note: async functions return wrapper promises even when a promise is returned, so equality checks should not be used.
+const p = new Promise((res, rej) => {
+  res(1);
+});
+async function asyncReturn() {
+  return p;
+}
+function basicReturn() {
+  // returns p since it is already a promise
+  return Promise.resolve(p);
+}
+console.log(p === basicReturn()); // true
+console.log(p === asyncReturn()); // false
+
+// async functions run snychronously up through the first await expression,
+// upon which execution will always continue asynchronously
+(async () => {
+  console.log("hi");
+  console.log("hi again");
+})();
+console.log("hi last");
+(async () => {
+  await console.log("hey");
+  console.log("hey again");
+})();
+console.log("hey NOT last");
+
+// Error handling should be considered carefully, because the handlers added to the async function's promise
+// will not account for any Promise created before/during the current await expression
+// that was not chained to or composited with the current await Promise. For example:
+async function foo() {
+  const p1 = new Promise((resolve) => setTimeout(() => resolve("1"), 1000));
+  const p2 = new Promise((_, reject) => setTimeout(() => reject("2"), 500));
+  // p2 is not part of any promise chain while awaiting the settling of p1
+  // const results = [await p1, await p2];
+  // this alternative line does NOT have the same issue:
+  await Promise.all([p1, p2]);
+}
+foo().catch(() => {}); // Attempt to swallow all errors, which fails
+
+// The expression after await is implicitly wrapped to make a promise:
+// 1. a native promise is returned as-is
+console.log(
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(10);
+    }, 200);
+  })
+);
+// 2. a thenable that is not a promise is converted to one via `new Promise((resolve, _) => thenable.then(resolve))`
+console.log(
+  await {
+    then(resolve, _reject) {
+      resolve("resolved!");
+    },
+  }
+);
+// 3. a non-thenable is wrapped in a fulfilled promise
+let obj = {};
+console.log((await obj) === obj);
+
+// use try-catch inside async functions to handle errors
+async function f4() {
+  try {
+    const z = await Promise.reject(new Error("async error"));
+  } catch (e) {
+    console.error(e.message);
+  }
+  // you could also catch errors via new Promise().catch(), but that won't catch the following:
+  try {
+    const zz = await Promise.resolve(
+      (() => {
+        throw new Error("synchronous error");
+      })()
+    );
+  } catch (e) {
+    console.error(e.message);
+  }
+  // Uncomment the following to see the difference:
+  // const zzz = await Promise.resolve(
+  //   (() => {
+  //     throw new Error("synchronous error");
+  //   })()
+  // ).catch((e) => {
+  //   // swallow error
+  // });
+}
+f4();
+
+// How top-level await in modules works:
+// 1. The execution of the current module is deferred until the awaited promise is resolved.
+// 2. The execution of the parent module is deferred until the child module that called await,
+//    and all its siblings, export bindings.
+// 3. The sibling modules, and siblings of parent modules, are able to continue executing in the same synchronous order —
+//    assuming there are no cycles or other awaited promises in the graph.
+// 4. The module that called await resumes its execution after the awaited promise resolves.
+// 5. The parent module and subsequent trees continue to execute in a synchronous order
+//    as long as there are no other awaited promises.
+export default await new Promise((resolve, _) => {
+  setTimeout(() => {
+    resolve("exported");
+  }, 100);
+});
+
+// queueMicrotask() pushes to the same queue (FIFO) as Promises (the microtask queue), so scheduling order is preserved.
+// The microtask queue performs all operations in the queue in between each task (macrotask) in the event loop.
+queueMicrotask(() => console.log("microtask async execute")); // added to queue
+(async () => {
+  console.log("async function synchronous start");
+  await null; // following lines are added to queue once this await expression is evaluated
+  console.log("async function async end");
+})();
+new Promise((resolve, _) => {
+  console.log("promise synchronous start");
+  resolve();
+}).then(() => console.log("promise async end")); // added to queue after promise resolves
+
+await null; // wait for prior example to finish
+// Wrap returns from async functions when explicitly returning a promise to preserve the stack trace (very small performance hit).
+async function asyncTask() {
+  await null;
+  throw new Error(`thrown at ${performance.now()}`);
+}
+async function withAwait() {
+  console.log("started withAwait()");
+  // remove await here to see that the thrown error does not include withAwait() in the stack trace
+  // because it was thrown after exiting the function
+  const task = await asyncTask();
+  console.log("ended withAwait()");
+  return task;
+}
+try {
+  withAwait();
+} catch (e) {
+  console.error(e);
+}
---
+++ learning_js/scripts/promises.js
@@ -0,0 +1,265 @@
+/*
+Example of chaining promises:
+
+doSomething()
+    .then((result) => doSomethingElse(result))
+    .then((newResult) => doThirdThing(newResult))
+    .then((finalResult) => {
+        console.log(`Got the final result: ${finalResult}`);
+    })
+    .catch(failureCallback);
+
+Note: The return value of a `then` callback can either be a Promise or non-Promise.
+- If it is a non-Promise, then the returned Promise will be fulfilled with that values
+- If it is a Promise, then the returned wrapper Promise will be settled with the fulfilled/rejected value of that Promise.
+Therefore, always make sure to return from a `then` callback since,  otherwise,
+the next `then` callback will receive `undefined` and will execute immediately,
+regardless of any pending Promises in the prior callback.
+
+Can also be written as:
+
+async function executePromises() {
+    try {
+        const result = await doSomething();
+        const newResult = await doSomethingElse(result);
+        const finalResult = await doThirdThing(newResult);
+        console.log(`Got the final result: ${finalResult}`);
+    } catch (error) {
+        failureCallback(error);
+    }
+}
+*/
+
+// You can chain `then` callbacks after an exception (because `catch` returns a Promise)
+Promise.resolve("Resolved")
+  .then(() => {
+    throw new Error("Something failed");
+  })
+  .catch(() => {
+    console.error("Do that");
+  })
+  .then(() => {
+    console.log("Do this, no matter what happened before");
+  });
+
+/*
+You can nest Promises if you need more specific error recovery, like the following:
+
+doSomethingCritical()
+  .then((result) =>
+    doSomethingOptional(result)
+      .then((optionalResult) => doSomethingExtraNice(optionalResult))
+      .catch((e) => result),
+  )
+  .then((result) => moreCriticalStuff(result))
+  .catch((e) => console.error(`Critical failure: ${e.message}`));
+*/
+
+// Event occurs when a Promise is rejected but there is no rejection handler attached.
+// Can be useful for debugging. Event will go to Worker if Promise was created in Worker.
+addEventListener("unhandledrejection", (event) => log(event.reason));
+onunhandledrejection = (event) => log(event.reason);
+
+// Event occurs when a rejection handler is added to a Promise after the Promise has already rejected.
+// Can be useful for debugging. Event will go to Worker if Promise was created in Worker.
+window.onrejectionhandled = (e) => log(event.reason);
+window.addEventListener("rejectionhandled", (e) => log(event.reason));
+
+// The following is the syntax for Node.js. This does override the default logging to the console though,
+// so make sure to implement some form of logging.
+process.on("unhandledRejection", (reason, promise) => {
+  // Add code here to examine the "promise" and "reason" values
+});
+
+// Promise composition methods. Promises all evaluate concurrently.
+//
+// .all()
+// Fulfills when ALL of the promises fulfill; rejects when ANY of the promises rejects with that initial reason.
+// Fulfillment value is the array of fulfillment values (including values that were not Promises).
+// Only ever runs synchronously if the iterable passed is empty, same for .allSettled() and .any().
+const p1 = Promise.resolve(3);
+const p2 = 1337;
+const p3 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("foo");
+  }, 100);
+});
+Promise.all([p1, p2, p3]).then((values) => {
+  console.log(values); // [3, 1337, "foo"]
+});
+
+// Good for avoiding chaining `await`s when the Promises are independent.
+//
+// async function getPrice() {
+//   const [choice, prices] = await Promise.all([promptForDishChoice(), fetchPrices()]);
+//   return prices[choice];
+// }
+//
+// VS
+//
+// async function getPrice() {
+//   const choice = await promptForDishChoice();
+//   const prices = await fetchPrices(); // wait for prior Promise to resolve before line is run
+//   return prices[choice];
+// }
+
+// You can avoid the fail-fast behavior of Promise.all() with specific error handling,
+// but probably prefer using Promise.allSettled().
+Promise.all([
+  Promise.resolve(setTimeout(() => resolve("success"), 1000)),
+  Promise.reject("error").catch((error) => error),
+]).then((values) => {
+  console.log(values[0]); // "success"
+  console.error(values[1]); // "Error: p2_immediate_rejection"
+});
+
+// .allSettled()
+// Fulfills when all promises SETTLE and does so with an array of objects representing all the settled Promises. Always fulfills.
+// Each object has a status property (either "fulfilled" or "rejected") and a `value` or `reason` property.
+const promise1 = Promise.resolve(3);
+const promise2 = new Promise((_, reject) => setTimeout(reject, 100, "foo"));
+const promises = [promise1, promise2];
+
+Promise.allSettled(promises).then((results) =>
+  results.forEach((result) => console.log(result.status))
+);
+
+// .any()
+// Fulfills when ANY of the promises fulfills and does so with the first fulfillment value;
+// rejects when ALL of the promises reject with an AggregateError,
+// which contains an array of rejection reasons in order of the PROMISED passed, NOT in order of the rejections of the errors.
+const pErr = new Promise((resolve, reject) => {
+  reject("Always fails");
+});
+const pSlow = new Promise((resolve, reject) => {
+  setTimeout(resolve, 500, "Done eventually");
+});
+const pFast = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "Done quick");
+});
+Promise.any([pErr, pSlow, pFast]).then((value) => {
+  console.log(value);
+});
+// Logs:
+// Done quick
+
+// Settles when ANY of the promises settles, with the settled result.
+// Always runs asynchronously, even if the iterable passed is empty (which will forever be pending).
+// If passed any already resolved Promises, the first passed will be used for the settled value.
+//
+// Useful for request timeouts
+const data = Promise.race([
+  fetch("/api"),
+  new Promise((resolve, reject) => {
+    // Reject after 5 seconds
+    setTimeout(() => reject(new Error("Request timed out")), 5000);
+  }),
+])
+  .then((res) => res.json())
+  .catch((err) => displayError(err));
+
+// Useful for fetching state of Promise since this cannot be done by inspecting the Promise object itself.
+function promiseState(promise) {
+  const pendingState = { status: "pending" };
+  // this runs asynchronously but is put immediately into the event queue so is as fast as possible for an async operation.
+  return Promise.race([promise, pendingState]).then(
+    (value) => (value === pendingState ? value : { status: "fulfilled", value }),
+    (reason) => ({ status: "rejected", reason })
+  );
+}
+
+// However, if you want synchronous running of Promises, you can do any of the following:
+//
+// [func1, func2, func3].reduce((p, f) => p.then(f), Promise.resolve()).then((result) => {});
+//
+// OR
+//
+// Promise.resolve()
+//   .then(func1)
+//   .then(func2)
+//   .then(func3)
+//   .then((result) => {});
+//
+// OR
+//
+// let result;
+// for (const f of [func1, func2, func3]) {
+//   result = await f(result);
+// }
+
+// Promises guarantees around timing:
+// - `then()` callbacks will never be invoked before the completion of the current run of the JavaScript event loop.
+// - The callbacks will be invoked even if they were added after the success or failure that the promise represents.
+// - Multiple callbacks may be added by calling `then()` several times. They will be invoked sequentially, in order.
+//
+// Promises are added to the microtask queue, which is processed until empty before the next task ('macrotask').
+const promise = new Promise((resolve, reject) => {
+  console.log("Promise callback");
+  resolve();
+}).then((result) => {
+  console.log("Promise callback (.then)");
+});
+
+// setTimeout goes on normal task queue since it is not a Promise API.
+setTimeout(() => {
+  console.log("event-loop cycle: Promise (fulfilled)", promise);
+}, 0);
+
+console.log("Promise (pending)", promise);
+// Logs:
+// Promise callback
+// Promise (pending) Promise {<pending>}
+// Promise callback (.then)
+// event-loop cycle: Promise (fulfilled) Promise {<fulfilled>}
+
+// Errors thrown inside asynchronous functions will act like uncaught errors
+let prom = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    throw new Error("Uncaught Exception!");
+  }, 1000);
+});
+prom.catch((e) => {
+  console.error(e); // This is never called
+});
+
+// Errors thrown after resolve is called will be silenced
+prom = new Promise((resolve, reject) => {
+  resolve();
+  throw new Error("Silenced Exception!");
+});
+prom.catch((e) => {
+  console.error(e); // This is never called
+});
+
+// .finally() can also be called, which returns a Promise that operates in the same vein as finally with try/catch.
+// Does not affect the value of the prior resolved Promise UNLESS if an error is thrown or it returns a rejected Promise.
+
+// Callback APIs can be wrapped with Promise constructor to make them into a Promise API, like so:
+function myAsyncFunction(url) {
+  return new Promise(
+    // This 'executor' function is run SYNCHRONOUSLY when the Promise is created (i.e., as the line is run).
+    // Throwing an error in the executor will reject the returned Promise the same as calling reject().
+    // The first call to reject(), resolve(), or throw will resolve the Promise and any subsequent calls will be ignored.
+    (resolve, reject) => {
+      // callback API
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      xhr.onload = () => resolve(xhr.responseText);
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+    }
+  );
+}
+
+// Note:
+// 'Resolved' means the value of the Promise is locked in, either fulfilled or rejected or pending on a further chained Promise.
+// 'Settled' means the Promise has been fulfilled or rejected.
+// Therefore, a Promise can be resolved but not settled if it is pending on the resolution of a further chained Promise.
+
+// Promise.withResolver() allows access to the resolving functions outside the executor scope, which allows some advanced use cases.
+// Note: this is not a standard API and very new.
+let { p, resolve, reject } = Promise.withResolvers();
+
+// .then() actually can take a second argument, which is a rejection handler,
+// but it is often better to use a final .catch() at the end of the chain (possibly with a .finally() afterward)
+// because a .catch().then() chain can be prone to errors, such as assuming the rejection value is the fulfillment value.
