9aa40ab84c86890dac9ac2189b5cab232630653e
Conrad Shock <shockconrad@gmail.com>
2024-02-29 12:58:34 -0800

    fix file after move

--- learning_js/pages/forms/form-sending-server.js
+++ learning_js/pages/forms/form-sending-server.js
@@ -2,6 +2,7 @@ import express from "express";
 import fs from "fs";
 import multer from "multer";
 import path from "path";
+import process from "process";
 
 // __dirname and __filename are not available in ES6 module
 const __dirname = path.resolve();
@@ -9,10 +10,10 @@ const app = express();
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     // for maximum safety, should ideally send file somewhere else that's isolated/sandboxed, for security
-    if (!fs.existsSync(__dirname + "/pages/forms/form-uploads")) {
-      fs.mkdirSync(__dirname + "/pages/forms/form-uploads");
+    if (!fs.existsSync(__dirname + "/form-uploads")) {
+      fs.mkdirSync(__dirname + "/form-uploads");
     }
-    cb(null, __dirname + "/pages/forms/form-uploads");
+    cb(null, __dirname + "/form-uploads");
   },
   // ideally would like to be able to set file to not have execute permission
   filename: function (req, file, cb) {
@@ -25,7 +26,7 @@ const storage = multer.diskStorage({
         "-" +
         file.originalname.substring(0, separator) +
         "." +
-        file.originalname.substring(separator + 1),
+        file.originalname.substring(separator + 1)
     );
   },
 });
@@ -38,26 +39,22 @@ const upload = multer({
 // express now comes bundled with its own body parser
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/pages/forms/form-sending", (req, res) => {
-  res.sendFile(__dirname + "/pages/forms/form-sending.html");
+app.get("/form-sending", (req, res) => {
+  res.sendFile(__dirname + "/form-sending.html");
 });
 
-const PORT = 8001;
+const PORT = process.env.PORT ?? 5500;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
 
-app.post("/pages/forms/form-sending", (req, res) => {
+app.post("/form-sending", (req, res) => {
   const { name, last } = req.body;
   res.send(`Thanks ${name} ${last}!`);
 });
 
-// not sure if express needs santization
-app.post(
-  "/pages/forms/form-sending/file-upload",
-  upload.single("file"),
-  (req, res) => {
-    const { name, last } = req.body;
-    res.send({ resp: `Thanks ${name} ${last}!` });
-  },
-);
+// not sure if express needs sanitization
+app.post("/form-sending/file-upload", upload.single("file"), (req, res) => {
+  const { name, last } = req.body;
+  res.send({ resp: `Thanks ${name} ${last}!` });
+});
