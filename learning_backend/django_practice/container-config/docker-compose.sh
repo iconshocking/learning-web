5284c270194695f3336a50e3fe448d5c6bc7bb4c
Conrad Shock <shockconrad@gmail.com>
2024-05-17 18:19:32 -0700

    requirements.txt note

--- learning_backend/django_practice/container-config/docker-compose.sh
+++ learning_backend/django_practice/container-config/docker-compose.sh
@@ -9,6 +9,9 @@ fi
 cd ..
 poetry export --without-hashes | awk '{ print $1 }' FS=';' > requirements.txt \
     && sed -i '' "s%.* @ file://$PWD%.%g" requirements.txt
+# alternatively, can use pip freeze if witin the poetry shell for a cleaner syntax
+#
+# pip freeze > requirements.txt && sed -i '' "s%.* @ file://$PWD%.%g" requirements.txt
 cd container-config
 
 if [ "$1" = "dev" ]; then
